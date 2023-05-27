a6c3715ea5eb122e68f9e69e3c39082f97f9d2e2
nikifrol <nikitosfrolik2005@gmail.com>
2023-05-27 18:11:09 +0300

    step 7 gcd

--- src/games/even.js
+++ src/games/even.js
@@ -1,17 +1,17 @@
 import runGameLogic from '../index.js';
-import getRandomNumber from '../utils.js';
+import genRandomNumber from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (num) => num % 2 === 0;
 
 const generateRound = () => {
-  const numQuestion = getRandomNumber(1, 50);
+  const numQuestion = genRandomNumber(1, 50);
   const answer = isEven(numQuestion) ? 'yes' : 'no';
   return [numQuestion, answer];
 };
 
 const runEvenGame = () => {
-    runGameLogic(description, generateRound);
+  runGameLogic(description, generateRound);
 };
 export default runEvenGame;
